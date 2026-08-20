6850ef79e38db2c37f466d3f0673642f68fc6317
VenkyGandrothu <gandrothuvenky@gmail.com>
2026-08-20 14:32:40 +0530

    url implemented

--- src/main/java/com/shortner/url/service/UrlService.java
+++ src/main/java/com/shortner/url/service/UrlService.java
@@ -1,29 +1,42 @@
 package com.shortner.url.service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import com.shortner.url.entity.Url;
-import com.shortner.url.repository.UrlRepo;
+import com.shortner.url.repository.UrlRepository;
 
 @Service
 public class UrlService {
 
-    @Autowired
-    private UrlRepo urlrepo;
+    private final UrlRepository urlRepository;
 
-    public Url saveUrl(Url url){
-        return urlrepo.save(url);
+    public UrlService(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
     }
 
-    public Optional<Url> findShortUrl(String shortCode){
-        return urlrepo.findByShortUrl(shortCode);
+    public Url createUrl(Url url) {
+        return urlRepository.save(url);
     }
 
-    public Optional<Url> findLongUrl(String longUrl){
-        return urlrepo.findByLongUrl(longUrl);
+    public Optional<Url> findByShortCode(String shortCode) {
+        return urlRepository.findByShortCode(shortCode);
     }
+
+    public Optional<Url> findByLongUrl(String longUrl) {
+        return urlRepository.findByLongUrl(longUrl);
+    }
+
+    public Url createUrl(String longUrl){
+        String shortCode = generateShortCode(longUrl);
+        Url url = new Url();
+        url.setLongUrl(longUrl);
+        url.setShortCode(shortCode);
+        url.setCreatedAt(LocalDateTime.now());
+        url.setUpdatedAt(LocalDateTime.now());
+        return urlRepository.save(url);
+    }
+
 }
